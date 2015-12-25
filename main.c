76574eda6cf19fbf781e8ff0b5ebd5e738bb4ab7
Colleen Murphy <colleen@gazlene.net>
2015-12-24 19:20:07 -0800

    Add support for ID3v2.3 tags

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ int find_start(FILE * fp)
   return 1;
 }
 
-int get_id3v1_tags(const char * file, char * title, char * artist, char * album)
+void get_id3v1_tags(const char * file, char * title, char * artist, char * album)
 {
   FILE *fp = fopen(file, "r");
   find_start(fp);
@@ -50,7 +50,97 @@ int get_id3v1_tags(const char * file, char * title, char * artist, char * album)
   read_tag(album, len, fp);
   printf("%s, %s, %s\n", title, artist, album);
   fclose(fp);
+}
+
+void read_id3v23_tag(char * buffer, const char * tag, FILE *fp)
+{
+  int read = 0;
+  int ind = 0;
+  int size;
+  char c;
+  int taglen = strlen(tag);
+  int maxread = 200;
+  while(read < maxread) {
+    while(ind < taglen) {
+      if((c = fgetc(fp)) != tag[ind]) {
+        if(c == 'T') {
+        }
+        ++read;
+        while(ind > 0) {
+          ungetc(c, fp);
+          --read;
+          --ind;
+        }
+        break;
+      }
+      ++ind;
+    }
+    if(ind == taglen) {
+      ind = 0;
+      while(ind < 3) {
+        fgetc(fp);
+        ++ind;
+        ++read;
+      }
+      int size = fgetc(fp);
+      fgetc(fp); fgetc(fp); // Eat flags
+      fgetc(fp); fgetc(fp); fgetc(fp); // Eat encoding descriptor
+      read = read + 6;
+      size = size - 5;
+      ind = 0;
+      int buffer_index = 0;
+      while(ind <= size) {
+        char c = fgetc(fp);
+        if(c != 0) {
+          buffer[buffer_index] = c;
+          ++buffer_index;
+        }
+        ++read;
+        ++ind;
+      }
+      buffer[buffer_index] = 0;
+      break;
+    }
+  }
+  if(read == maxread) {
+    printf("Failed to find tag.");
+  }
+}
+
+void get_id3v23_tags(const char * file, char * title, char * artist, char * album)
+{
+  FILE *fp = fopen(file, "r");
+  read_id3v23_tag(title, "TIT2", fp);
+  read_id3v23_tag(artist, "TPE1", fp);
+  read_id3v23_tag(album, "TALB", fp);
+  printf("%s, %s, %s\n", title, artist, album);
+  fclose(fp);
+}
+
+int is_id3v23(FILE *fp)
+{
+  fseek(fp, 0, SEEK_SET);
+  char buffer[4];
+  fgets(buffer, 4, fp);
+  if (strcmp(buffer, "ID3") == 0) {
+    if (fgetc(fp) == 3) {
+      return 1;
+    }
+  }
   return 0;
+}
+
+int get_format(const char * file)
+{
+  int format = -1;
+  FILE *fp = fopen(file, "r");
+  if (is_id3v23(fp)) {
+    format = 2;
+  } else {
+    format = 1;
+  }
+  fclose(fp);
+  return format;
 }
 
 int main(int argc, char ** argv)
@@ -62,6 +152,14 @@ int main(int argc, char ** argv)
     fprintf(stderr, "Must provide file path to read.\n");
     exit(1);
   }
-  get_id3v1_tags(argv[1], title, artist, album);
+  int format = get_format(argv[1]);
+  switch(format) {
+    case 1:
+      get_id3v1_tags(argv[1], title, artist, album);
+      break;
+    case 2:
+      get_id3v23_tags(argv[1], title, artist, album);
+      break;
+  }
   return 0;
 }
